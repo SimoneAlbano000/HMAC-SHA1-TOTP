8bfb0721ad5e628608e81d458af183574f276e2b
Simone Albano <36369471+SimoneAlbano000@users.noreply.github.com>
2023-05-24 14:51:52 +0200

    Update main.cpp

--- tests/sha1_platformIO/src/main.cpp
+++ tests/sha1_platformIO/src/main.cpp
@@ -1,13 +1,13 @@
 /*
 Computed Hash: (with -Os flag)
-RAM:   [          ]   3.5% (used 286 bytes from 8192 bytes)
-Flash: [          ]   2.3% (used 5798 bytes from 253952 bytes)
+RAM:   [-         ]   3.5% (used 286 bytes from 8192 bytes)
+Flash: [-         ]   2.3% (used 5798 bytes from 253952 bytes)
 84983E441C3BD26EBAAE4AA1F95129E5E54670F1
 119.77 H/s, in 0.834912 s
 
 Computed Hash: (with -O3 flag)
-RAM:   [          ]   3.5% (used 286 bytes from 8192 bytes)
-Flash: [          ]   4.4% (used 11200 bytes from 253952 bytes)
+RAM:   [-         ]   3.5% (used 286 bytes from 8192 bytes)
+Flash: [-         ]   4.4% (used 11200 bytes from 253952 bytes)
 84983E441C3BD26EBAAE4AA1F95129E5E54670F1
 121.49 H/s, in 0.823128 s
 */
