bb226ce3695ea8f2b436ed19a037e0f3ae9d6a0e
Simone Albano <36369471+SimoneAlbano000@users.noreply.github.com>
2023-05-25 09:53:51 +0200

    Update main.cpp

--- tests/TOTP_PlatformIO/src/main.cpp
+++ tests/TOTP_PlatformIO/src/main.cpp
@@ -1,14 +1,16 @@
 /*
 Performances with build_flags = -Os:
+    iterations = 100
     RAM:   [          ]   4.1% (used 332 bytes from 8192 bytes)
     Flash: [          ]   3.5% (used 8818 bytes from 253952 bytes)
-    Computed TOTP with epoch = 1, digit = 6: 755224
+    Computed TOTP with epoch = 00000001, key = "12345678901234567890", digit = 6: 755224
     TOTP per second: 59.22
     Execution time for 1 TOTP computation: 0.016885s
+    iterations = 100
 Performances with build_flags = -O3:
     RAM:   [          ]   4.1% (used 332 bytes from 8192 bytes)    
     Flash: [=         ]  10.1% (used 25634 bytes from 253952 bytes)
-    Computed TOTP with epoch = 1, digit = 6: 755224
+    Computed TOTP with epoch = 00000001, key = "12345678901234567890", digit = 6: 755224
     TOTP per second: 61.69
     Execution time for 1 TOTP computation: 0.016209s
 */
